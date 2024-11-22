dc1b2a8f57a3aa746e96bd6c971d5e9540edbb3e
Wiktoria Itrych <s190540@student.pg.edu.pl>
2024-11-22 11:07:39 +0100

    należy dokończyć wykres i imputacje

--- test_braku_danych.R
+++ test_braku_danych.R
@@ -10,6 +10,8 @@ library(Amelia)
 library(naniar)
 library(visdat)
 
+?amelia
+
 #wszystkie brakujące obserwacje - brakuje 400
 n_miss(HR)
 #udział brakujących w całości - 0.007774538 (0,7774%)
@@ -56,3 +58,47 @@ macierz_kor_brak <- cor_mat(
   alternative = "two.sided",
   conf.level = 0.95
 )
+
+#imputacja danych
+HR_imp <- amelia(HR, m = 5)
+#Amelia Error Code:  38 
+#The following variable(s) are characters: 
+#  Attrition (N), BusinessTravel (N), Department (N), EducationField (N), Gender (N), JobRole (N), MaritalStatus (N), Over18 (N), OverTime (N)
+#You may have wanted to set this as a ID variable to remove it
+#from the imputation model or as an ordinal or nominal
+#variable to be imputed.  Please set it as either and try again.
+HR_imp <- amelia(HR, m = 5, noms = c("Attrition", "BusinessTravel", "Department", "EducationField", "Gender", "JobRole", "MaritalStatus", "Over18", "OverTime"))
+#Amelia Error Code:  43 
+#You have a variable in your dataset that does not vary.  Please remove this variable. Variables that do not vary:  EmployeeCount, Over18, StandardHours 
+table(HR$EmployeeCount)
+table(HR$Over18)
+table(HR$StandardHours)
+#trzeba je usunąć
+HR_imp <- amelia(HR, m = 5, noms = c("Attrition", "BusinessTravel", "Department", "EducationField", "Gender", "JobRole", "MaritalStatus", "OverTime"), idvars = c("EmployeeCount", "Over18", "StandardHours"))
+
+#plot(HR_imp, which.vars = c("Age", "Attrition", "MonthlyIncome"))
+
+impu <- cbind(HR_imp$imputations[[1]], HR_imp$missMatrix)
+
+por_imp1 <- data.frame(age_orig = HR$Age, att_orig = HR$Attrition, moi_orig = HR$MonthlyIncome,
+                       age_impu = impu$Age, att_impu = impu$Attrition, moi_impu = impu$MonthlyIncome,
+                       mm = missMatrix)
+
+
+ggplot(por_imp1, aes(x = age_orig, y = age_impu, color = missing)) +
+  geom_point(alpha = 0.7) +
+  labs(title = "Porównanie oryginalnych i imputowanych wartości",
+       x = "Oryginalne wartości",
+       y = "Imputowane wartości") +
+  scale_color_manual(values = c("black", "red"), labels = c("Oryginalne", "Imputowane")) +
+  theme_minimal()
+
+# Wizualizacja różnic
+ggplot(comparison, aes(x = population_original, y = population_imputed, color = missing)) +
+  geom_point(alpha = 0.7) +
+  labs(title = "Porównanie oryginalnych i imputowanych wartości",
+       x = "Oryginalne wartości",
+       y = "Imputowane wartości") +
+  scale_color_manual(values = c("black", "red"), labels = c("Oryginalne", "Imputowane")) +
+  theme_minimal()
+is.na
