f212cea25f142b240873bb7295c509aeae50eee4
Wiktoria Itrych <s190540@student.pg.edu.pl>
2025-02-02 00:11:05 +0100

    analiza statystyczna, testy, niedokończone

--- statystyki_opisowe.R
+++ statystyki_opisowe.R
@@ -37,4 +37,56 @@ view(dfSummary(HR_filtered))
 ggbetweenstats(y = MonthlyIncome, 
                x = Attrition, 
                data = HR_imputowane)
+#-------------------------------------------------------------------------------
+install.packages("ggstatsplot")
+library(ggstatsplot)
+library(ggplot2)
+
+# Test chi-kwadrat: Zależność Attrition od Department
+# Czy odejścia pracowników zależą od działu, w którym pracują?
+
+ggbarstats(data = HR_filtered, x = Department, y = Attrition, title = "odejścia w zależności od działu firmy")
+
+
+
+#Test t-Studenta: Różnice w zarobkach między osobami, które odeszły i zostały
+#Czy pracownicy, którzy odchodzą, zarabiali mniej?
+
+ggbetweenstats(data = HR_filtered, x = Attrition, y = MonthlyIncome,  
+               title = "miesięczny dochód a odejścia z firmy")
+
+
+
+# ANOVA: Czy długość pracy w firmie różni się między działami?
+# Czy lata pracy zależą od działu?
+ggbetweenstats(data = HR_filtered, x = Department, y = YearsAtCompany)
+
+
+
+# Sprawdzenie korelacji między YearsAtCompany a MonthlyIncome
+# Czy osoby z dłuższym stażem zarabiają więcej?
+ggscatterstats(data = HR_filtered, x = YearsAtCompany, y = MonthlyIncome) +
+  scale_color_manual(values = c("#1B4F72", "#A3E4D7")) +  # Głębokie odcienie niebieskiego i zielonego
+  theme_minimal()
+
+
+
+#Struktura odejść w różnych działach
+#Jaki procent pracowników odchodzi z każdego działu?
+ggpiestats(data = HR_filtered, x = Department, y = Attrition) +
+  scale_fill_manual(values = c("#154360", "#1D8348", "#D4AC0D", "#935116")) +
+  theme_minimal()
+
+
+
+#Regresja logistyczna dla Attrition
+#Jakie czynniki przewidują odejście z firmy?
+
+ggcoefstats(glm(im_att_mice ~ Age + MonthlyIncome + YearsAtCompany + DistanceFromHome, 
+                data = HR_imputowane, family = binomial)) +
+  scale_fill_manual(values = c("#1D8348", "#935116", "#154360", "#D4AC0D")) + 
+  theme_minimal()
+
+#tylko tutaj trzeba chyba przekodować imputowane attrition na 0 i 1, bo jest 1 i 2
+
 
